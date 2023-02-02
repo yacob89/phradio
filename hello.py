2a695cb2ae58fc47e69f5476d2cd242a1e1065e9
Yacob Madiana <yacob.madiana@voicefoundry.com.au>
2023-02-02 15:28:59 +0700

    update name of books

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
+import book_of_bible
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template('index.html', name="Yacob")
 
+
 @app.route("/programs")
 def programs():
-    return render_template('programs.html')
+    return render_template('programs.html', new_testaments=book_of_bible.new_testaments, old_testaments=book_of_bible.old_testaments)
+
 
 if __name__ == "__main__":
     app.debug = True
